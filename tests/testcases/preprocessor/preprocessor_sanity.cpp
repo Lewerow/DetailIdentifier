c2db017cf3e9a79cfaf2e55d7105bbc4ce807d58
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2014-11-10 13:40:26 +0100

    Dixed preprocessor tests

--- tests/testcases/preprocessor/preprocessor_sanity.cpp
+++ tests/testcases/preprocessor/preprocessor_sanity.cpp
@@ -12,18 +12,24 @@ BOOST_AUTO_TEST_SUITE(preprocessor_tests)
 
 BOOST_AUTO_TEST_CASE(basic_sanity)
 {
-	BOOST_REQUIRE(boost::filesystem::exists(TEST_FILES_DIRECTORY_STRING + "preprocessor/in_sanity.jpg"));
+	const std::string input_path = TEST_FILES_DIRECTORY_STRING + "preprocessor/in_sanity.jpg";
+	const std::string output_path = TEST_FILES_DIRECTORY_STRING + "workspace/preprocessed.pgm";
 
+	BOOST_REQUIRE(boost::filesystem::exists(input_path));
+	if (boost::filesystem::exists(output_path))
+		boost::filesystem::remove(output_path);
+
+	BOOST_REQUIRE(!boost::filesystem::exists(output_path));
+	
 	mocks::logger log;
 	preprocessor::configuration config(boost::program_options::variables_map{}, log);
 	config.set_workspace(TEST_FILES_DIRECTORY_STRING + "workspace");
-	config.set_input_filename(TEST_FILES_DIRECTORY_STRING + "preprocessor/in_sanity.jpg");
+	config.set_input_filename(input_path);
 
 	preprocessor::preprocessor p(config);
 	auto out = p.preprocess(std::unique_ptr<preprocessor::input>());
 
-	BOOST_CHECK(boost::filesystem::exists(TEST_FILES_DIRECTORY_STRING + "workspace/preprocessed.pgm"));
-	BOOST_CHECK_EQUAL(TEST_FILES_DIRECTORY_STRING + "workspace/preprocessed.pgm", out->filename);
+	BOOST_CHECK(boost::filesystem::exists(out->filename));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
